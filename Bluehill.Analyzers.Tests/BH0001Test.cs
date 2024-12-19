04add4431525ebe1e0ddf7751a5fcafe6d35f72a
Jonghyo Lee <na1307@outlook.kr>
2024-12-19 11:44:18 +0900

    Test Test

---
+++ Bluehill.Analyzers.Tests/BH0001Test.cs
@@ -0,0 +1,10 @@
+﻿using VerifyCS = Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerVerifier<Bluehill.Analyzers.BH0001TypesShouldBeSealedAnalyzer, Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
+
+namespace Bluehill.Analyzers.Tests;
+
+public sealed class BH0001Test {
+    [Fact]
+    public Task TestTest() => VerifyCS.VerifyAnalyzerAsync("""
+        public class [|TestClass|];
+        """);
+}

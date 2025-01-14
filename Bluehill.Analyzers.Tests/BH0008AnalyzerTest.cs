f0f1e432dfba82ba81e5e9b2168ecc6114861464
Jonghyo Lee <na1307@outlook.kr>
2025-01-14 17:28:52 +0900

    BH0008 Test

---
+++ Bluehill.Analyzers.Tests/BH0008AnalyzerTest.cs
@@ -0,0 +1,106 @@
+﻿using Verify =
+    Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerVerifier<Bluehill.Analyzers.BH0008DontRepeatNegatedPatternAnalyzer,
+        Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
+
+namespace Bluehill.Analyzers.Tests;
+
+public sealed class BH0008AnalyzerTest {
+    [Theory]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(object? obj) {
+                _ = obj is [|not not|] null;
+            }
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(object? obj) {
+                _ = obj is [|not not not|] null;
+            }
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(TestEnum te) {
+                _ = te is [|not not|] TestEnum.One;
+            }
+        }
+
+        public enum TestEnum {
+            One,
+            Two,
+            Three
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(TestEnum te) {
+                _ = te is [|not not not|] TestEnum.One;
+            }
+        }
+
+        public enum TestEnum {
+            One,
+            Two,
+            Three
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(TestRecord tr) {
+                _ = tr is [|not not|] { Id: 0 };
+            }
+        }
+
+        public readonly record struct TestRecord(int Id, string Name);
+
+        namespace System.Runtime.CompilerServices {
+            public sealed class IsExternalInit;
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(TestRecord tr) {
+                _ = tr is [|not not not|] { Id: 0 };
+            }
+        }
+
+        public readonly record struct TestRecord(int Id, string Name);
+
+        namespace System.Runtime.CompilerServices {
+            public sealed class IsExternalInit;
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(string str) {
+                _ = str is [|not not|] [ 'C', '#' ];
+            }
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(string str) {
+                _ = str is [|not not not|] [ 'C', '#' ];
+            }
+        }
+        """
+        )]
+    public Task Test(string source) => Verify.VerifyAnalyzerAsync(source);
+}
---
+++ Bluehill.Analyzers.Tests/BH0008CodeFixProviderTest.cs
@@ -0,0 +1,179 @@
+using Verify =
+    Microsoft.CodeAnalysis.CSharp.Testing.CSharpCodeFixVerifier<Bluehill.Analyzers.BH0008DontRepeatNegatedPatternAnalyzer,
+        Bluehill.Analyzers.BH0008CodeFixProvider, Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
+
+namespace Bluehill.Analyzers.Tests;
+
+public sealed class BH0008CodeFixProviderTest {
+    [Theory]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(object? obj) {
+                _ = obj is [|not not|] null;
+            }
+        }
+        """, """
+        public class TestClass {
+            public void TestMethod(object? obj) {
+                _ = obj is null;
+            }
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(object? obj) {
+                _ = obj is [|not not not|] null;
+            }
+        }
+        """, """
+        public class TestClass {
+            public void TestMethod(object? obj) {
+                _ = obj is not null;
+            }
+        }
+        """
+        )]
+    // https://github.com/dotnet/roslyn-sdk/issues/1202
+    //[InlineData(
+    //    """
+    //    public class TestClass {
+    //        public void TestMethod(TestEnum te) {
+    //            _ = te is [|not not|] TestEnum.One;
+    //        }
+    //    }
+
+    //    public enum TestEnum {
+    //        One,
+    //        Two,
+    //        Three
+    //    }
+    //    """, """
+    //    public class TestClass {
+    //        public void TestMethod(TestEnum te) {
+    //            _ = te is TestEnum.One;
+    //        }
+    //    }
+
+    //    public enum TestEnum {
+    //        One,
+    //        Two,
+    //        Three
+    //    }
+    //    """
+    //    )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(TestEnum te) {
+                _ = te is [|not not not|] TestEnum.One;
+            }
+        }
+
+        public enum TestEnum {
+            One,
+            Two,
+            Three
+        }
+        """, """
+        public class TestClass {
+            public void TestMethod(TestEnum te) {
+                _ = te is not TestEnum.One;
+            }
+        }
+
+        public enum TestEnum {
+            One,
+            Two,
+            Three
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(TestRecord tr) {
+                _ = tr is [|not not|] { Id: 0 };
+            }
+        }
+
+        public readonly record struct TestRecord(int Id, string Name);
+
+        namespace System.Runtime.CompilerServices {
+            public sealed class IsExternalInit;
+        }
+        """, """
+        public class TestClass {
+            public void TestMethod(TestRecord tr) {
+                _ = tr is { Id: 0 };
+            }
+        }
+
+        public readonly record struct TestRecord(int Id, string Name);
+
+        namespace System.Runtime.CompilerServices {
+            public sealed class IsExternalInit;
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(TestRecord tr) {
+                _ = tr is [|not not not|] { Id: 0 };
+            }
+        }
+
+        public readonly record struct TestRecord(int Id, string Name);
+
+        namespace System.Runtime.CompilerServices {
+            public sealed class IsExternalInit;
+        }
+        """, """
+        public class TestClass {
+            public void TestMethod(TestRecord tr) {
+                _ = tr is not { Id: 0 };
+            }
+        }
+
+        public readonly record struct TestRecord(int Id, string Name);
+
+        namespace System.Runtime.CompilerServices {
+            public sealed class IsExternalInit;
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(string str) {
+                _ = str is [|not not|] [ 'C', '#' ];
+            }
+        }
+        """, """
+        public class TestClass {
+            public void TestMethod(string str) {
+                _ = str is [ 'C', '#' ];
+            }
+        }
+        """
+        )]
+    [InlineData(
+        """
+        public class TestClass {
+            public void TestMethod(string str) {
+                _ = str is [|not not not|] [ 'C', '#' ];
+            }
+        }
+        """, """
+        public class TestClass {
+            public void TestMethod(string str) {
+                _ = str is not [ 'C', '#' ];
+            }
+        }
+        """
+        )]
+    public Task Test(string source, string fixedSource) => Verify.VerifyCodeFixAsync(source, fixedSource);
+}

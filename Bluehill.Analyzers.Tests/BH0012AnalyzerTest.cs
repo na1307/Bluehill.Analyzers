5b350f78d2557ce4f2e1a7c9f001633935c6ac78
Jonghyo Lee <na1307@outlook.kr>
2025-02-03 03:11:56 +0900

    BH0012 Test

---
+++ Bluehill.Analyzers.Tests/BH0012AnalyzerTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Bluehill.Analyzers.Tests;
+
+public sealed class BH0012AnalyzerTest : BHAnalyzerTest<BH0012FieldNameShouldNotConflictAnalyzer> {
+    [Theory]
+    [InlineData("""
+                public class TestClass(int id) {
+                    private readonly int [|id|];
+                }
+                """)]
+    public Task Test(string source) => TestStatic(source);
+}

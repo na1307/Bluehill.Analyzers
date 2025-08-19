832802f6770f0112c40e7be292589063a7fafc60
Jonghyo Lee <na1307@outlook.kr>
2025-08-19 17:30:33 +0900

    Remove BH0016

--- Bluehill.Analyzers/EnumExtensionsGenerator.cs
+++ Bluehill.Analyzers/EnumExtensionsGenerator.cs
@@ -8,17 +8,13 @@ public sealed class EnumExtensionsGenerator : IIncrementalGenerator {
     public void Initialize(IncrementalGeneratorInitializationContext context) {
         var values = context.SyntaxProvider.ForAttributeWithMetadataName("Bluehill.EnumExtensionsAttribute",
             static (n, _) => n is EnumDeclarationSyntax,
-             (c, _) => ((CSharpCompilation)c.SemanticModel.Compilation).LanguageVersion >= LanguageVersion.CSharp10 ? GetEnumInfo((INamedTypeSymbol)c.TargetSymbol) : (EnumInfo?)null);
+            static (c, _) => GetEnumInfo((INamedTypeSymbol)c.TargetSymbol));
 
         context.RegisterSourceOutput(values, Generate);
     }
 
-    private static void Generate(SourceProductionContext context, EnumInfo? info) {
-        if (info is null) {
-            return;
-        }
-
-        var (fqnName, @namespace, accessibility, isFlags, members) = info.Value;
+    private static void Generate(SourceProductionContext context, EnumInfo info) {
+        var (fqnName, @namespace, accessibility, isFlags, members) = info;
         string accessibilityKeyword;
 
         try {
@@ -38,7 +34,8 @@ public sealed class EnumExtensionsGenerator : IIncrementalGenerator {
         }
 
         sb.Append(accessibilityKeyword).Append(" static class ").Append(GetEscapedName(typeName)).AppendLine("Extensions {")
-            .Append("    ").Append(accessibilityKeyword).Append(" static string ToStringFast(this ").Append(typeName).AppendLine(" value) => value switch {");
+            .Append("    ").Append(accessibilityKeyword).Append(" static string ToStringFast(this ").Append(typeName)
+            .AppendLine(" value) => value switch {");
 
         foreach (var member in members) {
             sb.Append("        ");
@@ -59,7 +56,8 @@ public sealed class EnumExtensionsGenerator : IIncrementalGenerator {
         sb.AppendLine("        _ => value.ToString()").AppendLine("    };");
 
         if (isFlags) {
-            sb.AppendLine().Append("    ").AppendLine("[System.Runtime.CompilerServices.MethodImplAttribute(256)]").Append("    ").Append(accessibilityKeyword).Append(" static bool HasFlagFast(this ").Append(typeName).Append(" value, ")
+            sb.AppendLine().Append("    ").AppendLine("[global::System.Runtime.CompilerServices.MethodImplAttribute(256)]").Append("    ")
+                .Append(accessibilityKeyword).Append(" static bool HasFlagFast(this ").Append(typeName).Append(" value, ")
                 .Append(typeName).AppendLine(" flag) => (value & flag) == flag;");
         }
 
@@ -72,9 +70,10 @@ public sealed class EnumExtensionsGenerator : IIncrementalGenerator {
         => new(enumSymbol.ToDisplayString(FqnFormat), enumSymbol.ContainingNamespace?.MetadataName, GetTypeAccessibility(enumSymbol),
             enumSymbol.HasAttribute(MetadataName.Parse("System.FlagsAttribute")), Array.AsReadOnly(enumSymbol.MemberNames.ToArray()));
 
-    private static string GetAccessibilityKeyword(Accessibility accessibility) => accessibility switch {
-        Accessibility.Public => "public",
-        Accessibility.Internal => "internal",
-        _ => throw new ArgumentOutOfRangeException(nameof(accessibility))
-    };
+    private static string GetAccessibilityKeyword(Accessibility accessibility)
+        => accessibility switch {
+            Accessibility.Public => "public",
+            Accessibility.Internal => "internal",
+            _ => throw new ArgumentOutOfRangeException(nameof(accessibility))
+        };
 }
